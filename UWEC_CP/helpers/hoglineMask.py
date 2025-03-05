17499df7fd45a732fb002385e4abadf11c2660bc
xavionbiesterveld <biestexm6907@uwec.edu>
2025-03-04 22:26:44 -0600

    Update hoglineMask.py

--- UWEC_CP/helpers/hoglineMask.py
+++ UWEC_CP/helpers/hoglineMask.py
@@ -1,11 +1,7 @@
 import cv2
-import numpy as np
-import matplotlib
-from matplotlib.pyplot import imshow
-from matplotlib import pyplot as plt
 
 #read image
-image = cv2.imread("file path here")
+image = cv2.imread("curlingimage1.png")
 
 #convert to grayscale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -14,13 +10,11 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 kernel_size = 3
 
 #i chose values between black and darker gray because I think thats the relative color of the hogline, if not we will have to test more
-ret,thresh = cv2.threshold(gray, 0, 30, cv2.THRESH_BINARY)
-
-cv2.imshow(gray)
+ret,thresh = cv2.threshold(gray, 165, 255, cv2.THRESH_BINARY_INV)
 
 #find contours
 contours = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-contours = countours[0] if len(contours) == 2 else countours[1]
+contours = contours[0] if len(contours) == 2 else contours[1]
 
 #draw countours
 radius = 2
@@ -28,7 +22,7 @@ color = (30, 255, 50)
 cv2.drawContours(image, contours, -1, color, radius)
 
 #show the masked image
-cv2.imshow(image)
-
-
-
+cv2.imshow('img', image)
+cv2.imshow('gray', gray)
+cv2.imshow('thresh', thresh)
+cv2.waitKey(0)
