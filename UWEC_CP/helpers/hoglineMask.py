2045a769d4284cbea271671149e5f435373e954b
xavionbiesterveld <biestexm6907@uwec.edu>
2025-03-04 22:50:47 -0600

    Update hoglineMask.py

--- UWEC_CP/helpers/hoglineMask.py
+++ UWEC_CP/helpers/hoglineMask.py
@@ -1,29 +1,33 @@
 import cv2
 
-#read image
+# Read image
 image = cv2.imread("curlingimage1.png")
 
-#convert to grayscale
+# Convert to grayscale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Apply histogram equalization
-equalized_image = cv2.equalizeHist(gray)
+# Apply CLAHE for better contrast
+clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
+equalized_image = clahe.apply(gray)
 
-#perform binary thresholding
-#i chose values between black and darker gray because I think thats the relative color of the hogline, if not we will have to test more
-ret,thresh = cv2.threshold(equalized_image, 165, 255, cv2.THRESH_BINARY_INV)
+# Adaptive thresholding
+thresh = cv2.adaptiveThreshold(equalized_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
+                               cv2.THRESH_BINARY_INV, 11, 20)
 
-#find contours
-contours = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-contours = contours[0] if len(contours) == 2 else contours[1]
+# Morphological operation to enhance the line
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+thresh = cv2.dilate(thresh, kernel, iterations=1)
 
-#draw countours
-radius = 2
+# Find contours
+contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+# Draw contours
 color = (30, 255, 50)
-cv2.drawContours(image, contours, -1, color, radius)
+cv2.drawContours(image, contours, -1, color, 2)
 
-#show the masked image
+# Show images
 cv2.imshow('img', image)
-cv2.imshow('gray', gray)
+cv2.imshow('gray', equalized_image)
 cv2.imshow('thresh', thresh)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
