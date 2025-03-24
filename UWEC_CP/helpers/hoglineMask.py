142cab00e0b1c953e2c5e39184afcaf0badcf6ae
BBeebster <153021388+BBeebster@users.noreply.github.com>
2025-03-24 15:08:19 -0500

    Update hoglineMask.py
    added line end detector for the hogline

--- UWEC_CP/helpers/hoglineMask.py
+++ UWEC_CP/helpers/hoglineMask.py
@@ -1,4 +1,9 @@
 import cv2
+import numpy as np
+from PIL import Image
+from scipy.ndimage import generic_filter
+from skimage.morphology import medial_axis
+from .assets import mask_hogline
 
 #read image
 image = cv2.imread("file path here")
@@ -34,3 +39,23 @@ cv2.waitKey(0)'''
 cv2.imwrite('curlingimage2_contours.png', image)
 cv2.imwrite('curlingimage2_thresh.png', thresh)
 cv2.destroyAllWindows()
+
+# Apply a mask on threshed image to isolate hogline
+mask = cv2.imread(mask_hogline)
+masked_thresh = cv2.bitwise_and(thresh, mask)
+
+# Skeletonize image (make lines 1 pixel thick
+skel = (medial_axis(masked_thresh)*255).astype(np.uint8)
+
+# Use line detection algorithm to determine end points of line
+
+# Line ends filter
+def lineEnds(P):
+    """Central pixel and just one other must be set to be a line end"""
+    return 255 * ((P[4]==255) and np.sum(P)==510)
+  
+# Find line ends
+result = generic_filter(skel, lineEnds, (3, 3))
+
+# Save result
+Image.fromarray(result).save('line_ends.png')
