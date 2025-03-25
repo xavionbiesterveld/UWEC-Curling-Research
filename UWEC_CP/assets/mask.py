1e7cf6b1d02f361542e9e89680bc8a34c4b222f9
BBeebster <153021388+BBeebster@users.noreply.github.com>
2025-03-24 19:25:57 -0500

    Update mask.py

--- UWEC_CP/assets/mask.py
+++ UWEC_CP/assets/mask.py
@@ -2,3 +2,4 @@ import os
 
 mask_a = os.path.join('UWEC_CP', 'assets', 'mask_a.png')
 mask_hogline = os.path.join('UWEC_CP', 'assets', 'curlingimage_thresh_mask.png')
+mask_hoglineB = os.path.join('UWEC_CP', 'assets', 'curlingimage_thresh_mask.png')
